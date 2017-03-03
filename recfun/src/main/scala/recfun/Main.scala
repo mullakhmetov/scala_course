ed2c53466568e0e55e07eaedb63a957763ca68b5
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2017-03-03 08:51:11 +0300

    Add Pascals's triangle and parentheses balancing solutions

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,49 @@
+package recfun
+
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(m: Int, n: Int): Int = {
+      def factorial(x: Int): Int = {
+        @tailrec
+        def inner(acc: Int, x: Int): Int = {
+          if (x==0) acc else inner(acc*x, x-1)
+        }
+        inner(1, x)
+      }
+      factorial(n) / (factorial(m) * factorial(n - m))
+    }
+
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = {
+      def inner (balance: Int, chars: List[Char]): Int = {
+        if (balance<0) balance
+        else chars match {
+          case Nil => balance
+          case '(' :: rest => inner(balance+1, rest)
+          case ')' :: rest => inner(balance-1, rest)
+          case head :: rest => inner(balance, rest)
+        }
+      }
+      inner(0, chars) == 0
+    }
+
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
